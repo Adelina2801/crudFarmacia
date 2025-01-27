eb642960d717ca50844dfde2bec24e26a5b5c598
Adelina_Santos <adevitoria.m09@gmail.com>
2025-01-27 10:11:17 -0300

    [200~https://github.com/Adelina2801/crudFarmacia.git~

---
+++ src/main/java/com/generation/novafarmacia/NovafarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.novafarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovafarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovafarmaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/novafarmacia/NovafarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.novafarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovafarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
